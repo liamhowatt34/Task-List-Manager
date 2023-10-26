14fec4066646594e3b6dca38d625a138151eae51
Liam Howatt <liamhowatt34@gmail.com>
2023-10-26 14:48:46 -0400

    Basic complete

--- main.cpp
+++ main.cpp
@@ -4,8 +4,25 @@
 using namespace std;
 
 int main() {
-    vector<vector<string>> to_do_list;
-    vector<vector<string>> completed_list;
+    vector<string> to_do_list;
+    vector<string> completed_list;
+    int number_of_tasks = 0;
+
+    cout << "How many tasks do you want to enter? ";
+    cin >> number_of_tasks;
+
+    for (int i = 0; i < number_of_tasks; i++) {
+        string task = "";
+        cout << "Enter task #" << i + 1 << ": ";
+        cin >> task;
+        to_do_list.push_back(task);
+    }
+
+    cout << endl << "To Do List: " << endl;
+
+    for (int i = 0; i < to_do_list.size(); i++) {
+        cout << i + 1 << ". " << to_do_list[i] << endl;
+    }
 
     return 0;
 }
